3c4af8bb1e36b43f9f5b5b7307e0c2e0d0d3b5b4
rubenduarte2905 <rubenduartemarecos@gmail.com>
2025-02-02 21:14:09 -0300

    Agregue App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Header from './components/layout/Header.js'; 
+import Nav from './components/layout/Nav.js';
+import Footer from './components/layout/Footer.js';
+
+import ContactoPage from './pages/ContactoPage.js'; 
+import HomePage from './pages/HomePage.js'; 
+import NosotrosPage from './pages/NosotrosPage.js'; 
+import NovedadesPage from './pages/NovedadesPage.js'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header/>
+
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="nosotros" element={<NosotrosPage />}/>
+          <Route path="novedades" element={<NovedadesPage />}/>
+          <Route path="contacto" element={<ContactoPage />}/>          
+        </Routes>
+      </BrowserRouter>
+
+
+      <Footer/>
+
     </div>
   );
 }
